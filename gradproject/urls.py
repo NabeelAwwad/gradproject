ecf8acc743cc4c9153999f3bd1ae2f96c3ffa531
NabeelAwwad <nabeelawwad3@gmial.com>
2022-01-12 06:24:35 +0200

    fix comp with heroku 23

--- gradproject/urls.py
+++ gradproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from learning import views as learning_views
 from django.contrib.auth import views as auth_views
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('student_register/', learning_views.student_register, name='student_register'),
     path('teacher_register/', learning_views.teacher_register, name='teacher_register'),
     path('', learning_views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', include("django.contrib.auth.urls"), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('quiz/', learning_views.quiz, name='quiz'),
     path('pref/', learning_views.poll, name='poll'),
