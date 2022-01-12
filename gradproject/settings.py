c1e48bdecece01518adb41f5e9fc2c8d3ab6a20f
NabeelAwwad <nabeelawwad3@gmial.com>
2022-01-12 03:55:49 +0200

    fix comp with heroku 11

--- gradproject/settings.py
+++ gradproject/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from django.contrib import messages
 import os
+
 import django_heroku
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
